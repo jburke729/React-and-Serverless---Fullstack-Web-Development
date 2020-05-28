b98945444b0fa2d73edac5454978fe42330b4046
jburke729 <junaid_burke@hotmail.com>
2020-05-28 10:25:30 -0400

    styled the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  StyledNavbar,
+  StyledNavBrand,
+  StyledNavItems,
+  StyledLink,
+} from "../styled/Navbar";
+
+import { Accent } from "../styled/Random";
 
 export default function Navbar() {
   return (
-    <div>
-      <Link to="/">
-        Learn.Build.<span>Type.</span>
-      </Link>
-      <ul>
+    <StyledNavbar>
+      <StyledNavBrand>
+        <Link to="/">
+          Learn.Build.<Accent>Type.</Accent>
+        </Link>
+      </StyledNavBrand>
+      <StyledNavItems>
         <li>
-          <Link to="/">Home</Link>
+          <StyledLink to="/">Home</StyledLink>
         </li>
         <li>
-          <Link to="game">Game</Link>
+          <StyledLink to="game">Game</StyledLink>
         </li>
         <li>
-          <Link to="highScores">High Scores</Link>
+          <StyledLink to="highScores">High Scores</StyledLink>
         </li>
         <li>
-          <Link to="gameOver">Game Over</Link>
+          <StyledLink to="gameOver">Game Over</StyledLink>
         </li>
-      </ul>
-    </div>
+      </StyledNavItems>
+    </StyledNavbar>
   );
 }
