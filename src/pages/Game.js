b642ae53af01262ee2ed7ad77b44a55f17120913
jburke729 <junaid_burke@hotmail.com>
2020-05-29 13:47:40 -0400

    based on input update user score

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,19 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from 'react';
 import {
   StyledGame,
   StyledScore,
   StyledTimer,
   StyledCharacter,
-} from "../styled/Game";
-import { Strong } from "../styled/Random";
+} from '../styled/Game';
+import { Strong } from '../styled/Random';
 
 export default function Game({ history }) {
   const [score, setScore] = useState(0);
   const MAX_SECONDS = 90;
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const [currentCharacter, setCurrentCharacter] = useState('');
   const [ms, setMs] = useState(0);
   const [seconds, setSeconds] = useState(MAX_SECONDS);
 
   useEffect(() => {
+    setRandomCharacter();
     const currentTime = new Date();
     const interval = setInterval(() => updateTime(currentTime), 1);
 
@@ -23,7 +26,7 @@ export default function Game({ history }) {
   const updateTime = (startTime) => {
     const endTime = new Date();
     const msPassedStr = (endTime.getTime() - startTime.getTime()).toString();
-    const formattedMSString = ("0000" + msPassedStr).slice(-5);
+    const formattedMSString = ('0000' + msPassedStr).slice(-5);
     // 00000 - first 2 are the seconds, last 3 are the ms that have passed
     const updatedSeconds =
       MAX_SECONDS - parseInt(formattedMSString.substring(0, 2)) - 1;
@@ -36,35 +39,50 @@ export default function Game({ history }) {
 
   useEffect(() => {
     if (seconds <= -1) {
-      history.push("/gameOver");
+      //Todo: save the score
+      history.push('/gameOver');
     }
   }, [seconds, ms, history]);
 
   const addLeadingZeros = (num, length) => {
-    let zeros = "";
+    let zeros = '';
     for (let i = 0; i < length; i++) {
-      zeros += "0";
+      zeros += '0';
     }
     return (zeros + num).slice(-length);
   };
-  const keyUpHandler = (e) => {
-    console.log(e.key);
+  const keyUpHandler = useCallback(
+    (e) => {
+      if (e.key === currentCharacter) {
+        setScore((prevScore) => prevScore + 1);
+      } else {
+        if (score > 0) {
+          setScore((prevScore) => prevScore - 1);
+        }
+      }
+      setRandomCharacter();
+    },[currentCharacter]);
+  
+    useEffect(() => {
+      document.addEventListener('keyup', keyUpHandler);
+      return () => {
+        document.removeEventListener('keyup', keyUpHandler);
+      };
+    }, [keyUpHandler]);
+
+  const setRandomCharacter = () => {
+    const randomInt = Math.floor(Math.random() * 36);
+    setCurrentCharacter(characters[randomInt]);
   };
-  useEffect(() => {
-    document.addEventListener("keyup", keyUpHandler);
-    return () => {
-      document.removeEventListener("keyup", keyUpHandler);
-    };
-  }, []);
 
   return (
     <StyledGame>
       <StyledScore>
         Score: <Strong>{score}</Strong>
       </StyledScore>
-      <StyledCharacter>A</StyledCharacter>
+      <StyledCharacter>{currentCharacter}</StyledCharacter>
       <StyledTimer>
-        Time:{" "}
+        Time:{' '}
         <Strong>
           {seconds}: {ms}
         </Strong>
