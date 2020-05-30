36972c1af14b8d654d7934694ad5a953947284b3
jburke729 <junaid_burke@hotmail.com>
2020-05-29 20:24:39 -0400

    increased game time

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -10,7 +10,7 @@ import { useScore } from '../contexts/ScoreContext';
 
 export default function Game({ history }) {
   const [score, setScore] = useScore();
-  const MAX_SECONDS = 5;
+  const MAX_SECONDS = 90;
   const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
   const [currentCharacter, setCurrentCharacter] = useState('');
   const [ms, setMs] = useState(0);
