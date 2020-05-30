f68b1856e540176b6ef5d924d5f6f56a9caf2fb6
jburke729 <junaid_burke@hotmail.com>
2020-05-30 15:57:39 -0400

    refactoring our serverless functions with an airtable utils file

--- src/pages/HighScores.js
+++ src/pages/HighScores.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import { ScoresList, ScoreLI } from '../styled/HighScores';
 
 export default function HighScores() {
+  //display those scores
+  const [highScores, setHighScores] = useState([]);
+
+  //use the fetch API to call getHighScores function
+  useEffect(() => {
+    const loadHighScores = async () => {
+      try {
+        const res = await fetch('/.netlify/functions/getHighScores');
+        const scores = await res.json();
+        setHighScores(scores);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    loadHighScores();
+  }, []);
+
   return (
     <div>
       <h1>HighScores</h1>
+      <ScoresList>
+        {highScores.map((score) => (
+          <ScoreLI key={score.id}>
+            {score.fields.name} - {score.fields.score}
+          </ScoreLI>
+        ))}
+      </ScoresList>
     </div>
   );
 }
