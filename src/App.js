b537e230f0a2d02304add801e32eeb8c7338b639
jburke729 <junaid_burke@hotmail.com>
2020-06-01 13:30:43 -0400

    using ThemeProvider from StyledComponents

--- src/App.js
+++ src/App.js
@@ -8,28 +8,35 @@ import Home from './pages/Home';
 
 import { Container } from './styled/Container';
 import { Main } from './styled/Main';
-import {GlobalStyle} from './styled/Global';
+import { GlobalStyle } from './styled/Global';
 import { useAuth0 } from './auth';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './styled/Themes';
 function App() {
   const { loading } = useAuth0();
+  const theme = 'light';
+  const currentTheme = theme === 'light' ? lightTheme : darkTheme;
+
   return (
     <Router>
-      <GlobalStyle />
-      <Main>
-        {loading && <p>Loading...</p>}
-        {!loading && (
-          <Container>
-            <Navbar />
-            <Switch>
-              <Route path="/game" component={Game} />
-              <Route path="/highScores" component={HighScores} />
-              <Route path="/gameOver" component={GameOver} />
-              <Route path="/" component={Home} />
-            </Switch>
-          </Container>
-        )}
-      </Main>
-      <GlobalStyle/>
+      <ThemeProvider theme={currentTheme}>
+        <GlobalStyle />
+        <Main>
+          {loading && <p>Loading...</p>}
+          {!loading && (
+            <Container>
+              <Navbar />
+              <Switch>
+                <Route path="/game" component={Game} />
+                <Route path="/highScores" component={HighScores} />
+                <Route path="/gameOver" component={GameOver} />
+                <Route path="/" component={Home} />
+              </Switch>
+            </Container>
+          )}
+        </Main>
+        <GlobalStyle />
+      </ThemeProvider>
     </Router>
   );
 }
